0685b556903d1de0d878b1f95c23244967d6508a
Belen <0cabezasbelen0@gmail.com>
2024-08-01 01:02:24 -0300

    casi terminado footer

--- tpfinal/src/Components/Footer/Footer.jsx
+++ tpfinal/src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex,Link,Image,Divider,Center} from "@chakra-ui/react";
+
 import fbcolor from "../../assets/Iconos/fbcolor.svg";
 import twcolor from "../../assets/Iconos/twcolor.svg";
 import igcolor from "../../assets/Iconos/igcolor.svg";
@@ -7,36 +8,49 @@ import "@fontsource/dm-serif-display";
 
 const Footer = () => {
   return (
-    <Box as="Footer" bg="#2A2344">
+    <Flex as="Footer" bg="#2A2344"
+    
+    >
       <Box
+
         as="Di-hola"
-        textAlign='center'
-        color='white'
+        
+        
       >
-        <Text
+        <Flex 
+        justifyContent='center'
+        color='white'
        fontSize="60px"
        fontFamily="dm-serif-display"
        pt="100px"
-       pb="20px"><h1>Dime hola</h1></Text>
-        <a href="mailto:0cabezasbelen0@gmail,com">
-          <p>0cabezasbelen0@gmail.com</p>
-        </a>
-        <Box as="IconFinal"
+       pb="20px"><h1>Dime hola</h1></Flex>
+
+
+<Link display='flex' justifyContent='center' color='white' href='mailto:0cabezasbelen0@gmail,com'>
+0cabezasbelen0@gmail.com
+  </Link>
+        
+        <Flex as="IconFinal"
+        
         pb='60px'>
           <a href="https://www.facebook.com/profile.php?id=100011000144769">
-            <img src={fbcolor} />
+            <Image p='30px 20px' src={fbcolor} />
           </a>
           <a href="https://x.com/be3elu">
-            <img src={twcolor} />
+            <Image 
+            p='30px 20px' src={twcolor} />
           </a>
           <a href="https://ar.linkedin.com/">
-            <img src={igcolor} />
+            <Image p='30px 20px' src={igcolor} />
           </a>
-        </Box>
+        </Flex>
       
-      </Box>
+        </Box>
 
-      <Box
+    
+
+      <Flex
+
         as="Copy"
         justifyContent="center"
         fontSize="20px"
@@ -46,8 +60,10 @@ const Footer = () => {
         {/*<!--Icono copy-->*/}
         <i class="bi bi-c-circle"></i>
         <p>2022-Form|Todos los derechos resevados</p>
-      </Box>
-    </Box>
+        </Flex>
+        
+      
+    </Flex>
   );
 };
 
