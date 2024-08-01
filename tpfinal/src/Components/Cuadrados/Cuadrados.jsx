b2f9956fa44521bbdcb98781598277113622ef1c
Belen <0cabezasbelen0@gmail.com>
2024-07-31 22:12:07 -0300

    editado  experiencia y proyectos

--- tpfinal/src/Components/Cuadrados/Cuadrados.jsx
+++ tpfinal/src/Components/Cuadrados/Cuadrados.jsx
@@ -3,77 +3,112 @@ import '@fontsource/roboto';
 import { Box, Flex } from "@chakra-ui/react";
 
 const Cuadrados=()=>{
-    return(
-      <Flex class="Cuadrados"
-      justifyContent='space-around'
-      fontFamily='roboto'
-      color='white'
-      pt='60px'
-      pb='60px'
-      bg='#2A2344'>
-
-        <Box className="Premio"
-        border='3px dashed #D5223A'
-        borderTopRightRadius='45px'
-        m='30px 0px'
-        w='350px'
-        fontSize='30px'
-        textAlign='center'
-        pb='40px'
+    return (
+      <Flex
+        class="Cuadrados"
+        justifyContent="space-around"
+        fontFamily="roboto"
+        color="white"
+        pt="60px"
+        pb="60px"
+        bg="#2A2344"
+      >
+        <Box
+          className="Premio"
+          border="3px dashed #D5223A"
+          borderTopRightRadius="45px"
+          m="30px 0px"
+          w="350px"
+          fontSize="30px"
+          textAlign="center"
+          pb="40px"
         >
-       {/*!--Primer cuadrado--*/}
-          <Box 
-          position='relative'
-          top='-23px'
-          left='45px'
-          bg='#D5223A'
-          width='45px'
-          p='10px'
-          > <i class="bi bi-trophy"></i></Box>
-       <h1>0+</h1>
-       <h2>Premios</h2>
+          {/*!--Primer cuadrado--*/}
+          <Box
+            position="relative"
+            top="-23px"
+            left="45px"
+            bg="#D5223A"
+            width="45px"
+            p="10px">
+            
+            <i class="bi bi-trophy"></i>
+          </Box>
+          <h1>0+</h1>
+          <h2>Premios</h2>
         </Box>
-        
-    <Box className="Clientes"
-    border='3px solid #D5223A'
-    borderTopRightRadius='45px'
-    m='30px 0px'
-    w='350px'
-    fontSize='30px'
-    textAlign='center'
-    pb='40px'
-    >
-      {/*!--Segundo cuadrado--*/}
-         <Box
-          position='relative'
-          top='-23px'
-          left='45px'
-          bg='#D5223A'
-          width='45px'
-          p='10px'
-         > <i class="bi bi-people"></i></Box>
-      <h1>0+</h1>
-      <h2>Clientes</h2>
-    </Box>
-       
-     <Box class="Exp">
-      {/*!--Tercer cuadrado--*/}
-     <Box><i class="bi bi-person-lines-fill"></i></Box> 
-      <h1>01+</h1>
-      <h2>Meses de experiencia</h2>
-     </Box>
-      
-    <Box class="Proyectos">
-      {/*!--Cuarto cuadrado--*/}
-      <i class="bi bi-pc-display"></i>
-      <h1>04+</h1>
-      <h2>Proyectos</h2>
-    </Box>
-      
+
+        <Box
+          className="Clientes"
+          border="3px solid #D5223A"
+          borderTopRightRadius="45px"
+          m="30px 0px"
+          w="350px"
+          fontSize="30px"
+          textAlign="center"
+          pb="40px">
+
+          {/*!--Segundo cuadrado--*/}
+          <Box
+            position="relative"
+            top="-23px"
+            left="45px"
+            bg="#D5223A"
+            width="45px"
+            p="10px">
+            <i class="bi bi-people"></i>
+          </Box>
+          <h1>0+</h1>
+          <h2>Clientes</h2>
+        </Box>
+
+        <Box
+          className="Exp"
+          border="3px dashed #D5223A"
+          borderTopRightRadius="45px"
+          m="30px 0px"
+          w="350px"
+          fontSize="30px"
+          textAlign="center"
+          pb="40px">
+          {/*!--Tercer cuadrado--*/}
+          <Box
+            position="relative"
+            top="-23px"
+            left="45px"
+            bg="#D5223A"
+            width="45px"
+            p="10px">
+            <i className="bi bi-person-lines-fill"></i>
+          </Box>
+          <h1>01+</h1>
+          <h2>Meses de experiencia</h2>
+        </Box>
+
+        <Box
+          className="Proyectos"
+          border="3px dashed #D5223A"
+          borderTopRightRadius="45px"
+          m="30px 0px"
+          w="350px"
+          fontSize="30px"
+          textAlign="center"
+          pb="40px">
+          {/*!--Cuarto cuadrado--*/}
+          <Box
+            position="relative"
+            top="-23px"
+            left="45px"
+            bg="#D5223A"
+            width="45px"
+            p="10px">
+            <i className="bi bi-pc-display"></i>
+          </Box>
+          <h1>04+</h1>
+          <h2>Proyectos</h2>
+        </Box>
       </Flex>
-     
-       
-    )
+    );
 }
 
 export {Cuadrados}
