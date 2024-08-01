903d78e169532c555d432d7797528fcba27f8502
Belen <0cabezasbelen0@gmail.com>
2024-08-01 19:26:14 -0300

    hice las tabs para imagenes

--- tpfinal/src/Components/Portfolio/Portfolio.jsx
+++ tpfinal/src/Components/Portfolio/Portfolio.jsx
@@ -3,66 +3,46 @@ import tpo1 from "../../assets/Imagenes/tpo 1.png";
 import tpo1b from "../../assets/Imagenes/tpo 1b.png";
 import tpoa from "../../assets/Imagenes/tpo a.png";
 import tpob from "../../assets/Imagenes/tpo b.png";
-import { Tabs,TabPanels,TabPanel,Image,Tab,TabList} from "@chakra-ui/react";
+import {
+  Tabs,
+  TabPanels,
+  TabPanel,
+  Image,
+  Tab,
+  TabList,
+} from "@chakra-ui/react";
 
-const Portfolio =()=>{
-    return(
-      
-      <section>
+const Portfolio = () => {
+  return (
+    <section>
       <div class="Portafolio-Proyecto">
-    <div class="Portafolio">
-        <h2>Portafolio</h2>
-        <p>Estos son algunos de mis trabajos</p>
-    </div>
-    <div class="Proyectos2">
-        <a href="">Todos los trabajos</a>
-        <a href="">Proyecto personal</a>
-        <a href="">Proyecto profesional</a>
-    </div>
-      </div>  
-    
+        <div class="Portafolio">
+          <h2>Portafolio</h2>
+          <p>Estos son algunos de mis trabajos</p>
+        </div>
+      </div>
+
       <Tabs defaultIndex={1}>
-  <TabPanels>
-    <TabPanel>
-      <Image
-        boxSize='200px'
-        fit='cover'
-        src={tpo1}
-      />
-    </TabPanel>
-    <TabPanel>
-      <Image
-        boxSize='200px'
-        fit='cover'
-        src={tpo1b}
-      />
-    </TabPanel>
-  </TabPanels>
-  <TabList>
-    <Tab>Naruto</Tab>
-    <Tab>Sasuke</Tab>
-  </TabList>
-</Tabs>
-
-
-    <div class="Trabajos">
-        {/*<!--Tpo 1-->*/}
-        <img src={tpo1} />
-        {/*<!--Tpo 2-->*/}
-        <img src= {tpo1b} />
-        {/*<!--Tpo 3-->*/}
-        <img src= {tpoa} />
-        {/*<!--Tpo 4-->*/}
-        <img src= {tpob} />
-    </div>
-    <div class="Btn">
-      <button>Ver todos</button>
-    </div>
-    
+        <TabList>
+        <Tab _selected={{ color: 'white', bg:'#D6223B'}}>Todos los trabajos</Tab>
+        <Tab _selected={{ color: 'white', bg:'#D6223B'}}>Proyecto personal</Tab>
+        <Tab isDisabled>Proyecto profesional</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Image boxSize="200px" fit="cover" src={tpo1} />
+            <Image boxSize="200px" fit="cover" src={tpo1b} />
+            <Image boxSize="200px" fit="cover" src={tpoa} />
+            <Image boxSize="200px" fit="cover" src={tpob} />
+          </TabPanel>
+          <TabPanel>
+            <Image boxSize="200px" fit="cover" src={tpoa} />
+            <Image boxSize="200px" fit="cover" src={tpob} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </section>
-        
-       
-    )
-}
+  );
+};
 
-export {Portfolio}
+export { Portfolio };
