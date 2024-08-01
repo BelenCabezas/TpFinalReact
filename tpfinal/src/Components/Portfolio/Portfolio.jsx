c9eb1630918744bbaffafa8eaae65c743b3d09da
Belen <0cabezasbelen0@gmail.com>
2024-08-01 20:33:45 -0300

    edite portfolio

--- tpfinal/src/Components/Portfolio/Portfolio.jsx
+++ tpfinal/src/Components/Portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import tpo1b from "../../assets/Imagenes/tpo 1b.png";
 import tpoa from "../../assets/Imagenes/tpo a.png";
 import tpob from "../../assets/Imagenes/tpo b.png";
 import "@fontsource/cactus-classical-serif";
+import '@fontsource/roboto';
 import {
   Tabs,
   TabPanels,
@@ -14,11 +15,24 @@ import {
   Flex,
   Box,
   Text,
+  Button,
+  TabIndicator,
 } from "@chakra-ui/react";
 
 const Portfolio = () => {
   return (
-    
+    <Box><Flex flexDirection='column'
+    bg="#19142A"
+    color="white"
+    p="40px"
+    fontFamily="cactus-classical-serif"
+    fontWeight='lighter'
+    as="Portafolio">
+        <Text fontSize="50px">Portafolio</Text>
+        <Text margin="20px 0px" fontSize="20px">
+          Estos son algunos de mis trabajos
+        </Text>
+      </Flex>
     <Flex
       justifyContent="space-around"
       bg="#19142A"
@@ -26,16 +40,11 @@ const Portfolio = () => {
       p="40px"
       fontFamily="cactus-classical-serif"
     >
-      <Box as="Portafolio">
-        <Text fontSize="50px">Portafolio</Text>
-        <Text margin="20px 0px" fontSize="20px">
-          Estos son algunos de mis trabajos
-        </Text>
-      </Box>
+      
 
-      <Tabs defaultIndex={2}>
-        <TabList>
-          <Tab _selected={{ color: "white", bg: "#D6223B" }}>
+      <Tabs variant='unstyled'  >
+        <TabList >
+          <Tab  _selected={{ color: "white", bg: "#D6223B" }}>
             Todos los trabajos
           </Tab>
           <Tab _selected={{ color: "white", bg: "#D6223B" }}>
@@ -43,28 +52,39 @@ const Portfolio = () => {
           </Tab>
           <Tab isDisabled>Proyecto profesional</Tab>
         </TabList>
+        
         <TabPanels>
           <TabPanel>
             <Flex justifyContent='center'
-            flexWrap='wrap'
-           
-            >
-            < Image p='10px 5px' alt="Primer pagina web" boxSize="md"  src={tpo1} />
-            <Image p='10px 5px' alt="Seccion de arte" boxSize="md"  src={tpo1b} />
-            <Image p='10px 5px' alt="Copia de servicios" boxSize="md"  src={tpoa} />
-            <Image p='10px 5px' alt="Copia de registro" boxSize="md"  src={tpob} /></Flex>
+            flexWrap='wrap'>
+            < Image p='10px 5px' alt="Primer pagina web" boxSize="xl"  src={tpo1} />
+            <Image p='10px 5px' alt="Seccion de arte" boxSize="xl"  src={tpo1b} />
+            <Image p='10px 5px' alt="Copia de servicios" boxSize="xl"  src={tpoa} />
+            <Image p='10px 5px' alt="Copia de registro" boxSize="xl"  src={tpob} /></Flex>
           </TabPanel>
           <TabPanel>
           <Flex justifyContent='center'
-            flexWrap='wrap'
-            >
-            <Image p='10px 5px' alt="Copia de servicios" boxSize="md"  src={tpoa} />
-            <Image p='10px 5px' alt="Copia de registro" boxSize="md"  src={tpob} /></Flex>
+            flexWrap='wrap'>
+            <Image p='10px 5px' alt="Copia de servicios" boxSize="xl"  src={tpoa} />
+            <Image p='10px 5px' alt="Copia de registro" boxSize="xl"  src={tpob} /></Flex>
           </TabPanel>
         </TabPanels>
       </Tabs>
+      
     </Flex>
-    
+    <Flex justifyContent='center'
+    bg="#19142A">
+      <Button _hover={{ bg: "#D6223B" }} color='white'
+      fontFamily='roboto'
+      bg= "#D6223B"
+      fontSize='30px'
+      fontWeight='lighter'
+      p='40px'
+      mt='55px'
+      mb='70px'
+      >Ver todos</Button>
+      </Flex>
+  </Box>
   );
 };
 
