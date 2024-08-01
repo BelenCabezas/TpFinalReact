6ac2d9367a46e9d8ab15a02e6c6eaf63cf34da8d
Belen <0cabezasbelen0@gmail.com>
2024-08-01 19:10:49 -0300

    editando portfolio tabs

--- tpfinal/src/Components/Portfolio/Portfolio.jsx
+++ tpfinal/src/Components/Portfolio/Portfolio.jsx
@@ -1,41 +1,67 @@
 import React from "react";
-
 import tpo1 from "../../assets/Imagenes/tpo 1.png";
 import tpo1b from "../../assets/Imagenes/tpo 1b.png";
 import tpoa from "../../assets/Imagenes/tpo a.png";
 import tpob from "../../assets/Imagenes/tpo b.png";
+import { Tabs,TabPanels,TabPanel,Image,Tab,TabList} from "@chakra-ui/react";
 
 const Portfolio =()=>{
     return(
-        <section>
-          <div className="Portafolio-Proyecto">
-        <div className="Portafolio">
-            <h2>Portafolio</h2>
-            <p>Estos son algunos de mis trabajos</p>
-        </div>
-        <div className="Proyectos2">
-            <a href="">Todos los trabajos</a>
-            <a href="">Proyecto personal</a>
-            <a href="">Proyecto profesional</a>
-        </div>
-          </div>  
-        
+      
+      <section>
+      <div class="Portafolio-Proyecto">
+    <div class="Portafolio">
+        <h2>Portafolio</h2>
+        <p>Estos son algunos de mis trabajos</p>
+    </div>
+    <div class="Proyectos2">
+        <a href="">Todos los trabajos</a>
+        <a href="">Proyecto personal</a>
+        <a href="">Proyecto profesional</a>
+    </div>
+      </div>  
+    
+      <Tabs defaultIndex={1}>
+  <TabPanels>
+    <TabPanel>
+      <Image
+        boxSize='200px'
+        fit='cover'
+        src={tpo1}
+      />
+    </TabPanel>
+    <TabPanel>
+      <Image
+        boxSize='200px'
+        fit='cover'
+        src={tpo1b}
+      />
+    </TabPanel>
+  </TabPanels>
+  <TabList>
+    <Tab>Naruto</Tab>
+    <Tab>Sasuke</Tab>
+  </TabList>
+</Tabs>
 
-        <div className="Trabajos">
-            {/*<!--Tpo 1-->*/}
-            <img src= {tpo1}/>
-           { /*<!--Tpo 2-->*/}
-            <img src={tpo1b}/>
-            {/*<!--Tpo 3-->*/}
-            <img src={tpoa}/>
-           { /*<!--Tpo 4-->*/}
-            <img src={tpob}/>
-        </div>
-        <div className="Btn">
-          <button>Ver todos</button>
-        </div>
+
+    <div class="Trabajos">
+        {/*<!--Tpo 1-->*/}
+        <img src={tpo1} />
+        {/*<!--Tpo 2-->*/}
+        <img src= {tpo1b} />
+        {/*<!--Tpo 3-->*/}
+        <img src= {tpoa} />
+        {/*<!--Tpo 4-->*/}
+        <img src= {tpob} />
+    </div>
+    <div class="Btn">
+      <button>Ver todos</button>
+    </div>
+    
+    </section>
         
-        </section>
+       
     )
 }
 
