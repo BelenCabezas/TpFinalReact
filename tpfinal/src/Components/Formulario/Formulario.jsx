3dca74d481d306d1bb22ac2d42821e359a745afa
Belen <0cabezasbelen0@gmail.com>
2024-08-01 21:13:10 -0300

    estilando form

--- tpfinal/src/Components/Formulario/Formulario.jsx
+++ tpfinal/src/Components/Formulario/Formulario.jsx
@@ -2,176 +2,89 @@ import React from "react";
 import telefono from "../../assets/Iconos/telefono.png";
 import email from "../../assets/Iconos/email.png";
 import location from "../../assets/Iconos/location.png";
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Text,Img } from "@chakra-ui/react";
 import "@fontsource/dm-serif-display";
 
 const Formulario = () => {
   return (
-    <Box as="Form" bg="#19142A" color="white" pt="80px">
-      <Flex as="Completo" justifyContent="space-around">
-        <Box as="Contactame" pl="10px">
-          <Text
-            fontSize="40px"
-            fontWeight="lighter"
-            fontFamily="dm-serif-display"
-          >
-            Contactame
-          </Text>
-          <Text fontSize="20px" fontWeight="10px" pt="20px">
-            Hagamos algo nuevo,diferente y mas significativo o hamos algo mas
-            visual o conceptual?
-          </Text>
+    <Box as="Form"
+    bg="#19142A"
+     color="white" 
+     pt="80px">
 
-          <div className="Llamame">
-            <Box
-              position="relative"
+  <Flex as="Completo"
+  justifyContent='space-around'>
+    <Box as="Contactame"
+    pl='10px'>
+      <Text
+      fontSize='40px'
+      fontFamily='dm-serif-display'>Contactame</Text>
+      <Text fontSize='20px'
+      pt='20px'
+      fontWeight='10px'>
+        Hagamos algo nuevo,diferente y mas significativo o hamos algo mas visual o conceptual?</Text>
+
+     <div class="Llamame">
+      <Img position="relative"
               top="53px"
               right="56px"
-              w="24px"
-              bg="	#D5223A"
-              borderRadius="18px"
-              p="8px"
-            >
-              <img src={telefono} />
-            </Box>
-            <Text color="gray" fontSize="20px">
-              Llamame
-            </Text>
-            <Text fontSize="25px">+541167228883</Text>
-          </div>
-
-          <div className="Email">
-            <Box
-              position="relative"
-              top="53px"
-              right="56px"
-              w="24px"
-              bg="	#D5223A"
-              borderRadius="18px"
-              p="8px"
-            >
-              <img src={email} />
-            </Box>
-            <Text color="gray" fontSize="20px">
-              Email
-            </Text>
-            <Text fontSize="25px">0cabezasbelen0@gmail.com</Text>
-          </div>
-
-          <div className="Direccion">
-            <Box
-              position="relative"
-              top="53px"
-              right="56px"
-              w="24px"
-              bg="	#D5223A"
-              borderRadius="18px"
-              p="8px"
-            >
-              <img src={location} />
-            </Box>
-            <Text color="gray" fontSize="20px">
-              Direccion
-            </Text>
-            <Text fontSize="25px">Gonzalez catan,Bs As.</Text>
-          </div>
-        </Box>
-
-        <Flex as="Formulario" flexDirection="column" mb="80px">
-          <Text
-            fontSize="40px"
-            fontWeight="lighter"
-            fontFamily="dm-serif-display"
-            pb="20px"
-          >
-            Dejame tu consulta
-          </Text>
-          <div className="Formulario-completo">
-            <Box as="NombreEmail" >
-              <Box 
-                bg="	#19142A"
-                color="gray"
-                border="solid gray 1px"
-                w="200px"
-                h="40px"
-                borderRadius="5px"
-                m="10px"
-              >
-                <input placeholder="Nombre" type="text" />
-              </Box>
-              <Box
-                bg="	#19142A"
-                color="gray"
-                border="solid gray 1px"
-                w="200px"
-                h="40px"
-                borderRadius="5px"
-                m="10px"
-              >
-                <input placeholder="Email" type="email" />
-              </Box>
-            </Box>
-            <div className="NumeroPresupuesto">
-              <Box
-                bg="	#19142A"
-                color="gray"
-                border="solid gray 1px"
-                w="200px"
-                h="40px"
-                borderRadius="5px"
-                m="10px"
-              >
-                <input placeholder="Numero" type="tel" />
-              </Box>
-              <Box
-                bg="	#19142A"
-                color="gray"
-                border="solid gray 1px"
-                w="200px"
-                h="40px"
-                borderRadius="5px"
-                m="10px"
-              >
-                <input placeholder="Presupuesto" type="text" />
-              </Box>
-            </div>
-
-            <Box
-              bg="	#19142A"
-              color="gray"
-              border="solid gray 1px"
-              w="437px"
-              h="107"
-              borderRadius="5px"
-              position="relative"
-              left="7px"
-            >
-              <input
-                className="Mensaje"
-                placeholder="Mensaje"
-                minlength="10"
-                type="text"
-              />
-            </Box>
-
-            <Button
-              display="flex"
-              justifyContent="start"
-              mt="40px"
+              w="40px"
               bg="#D5223A"
-              color="white"
-              borderRadius="5px"
-              py="20px"
-              px="50px"
-              border="none"
-            >
-              
-              Enviar
-            </Button>
-          </div>
-        </Flex>
-      </Flex>
+              borderRadius="18px"
+              p="8px" src={telefono} />
+        <Text color='gray'
+        fontSize='20px'>Llamame</Text>
+        <Text fontSize='25px'>+541167228883</Text>
+     </div>
 
+      <div class="Email">
+        <Img position="relative"
+              top="53px"
+              right="56px"
+              w="40px"
+              bg="#D5223A"
+              borderRadius="18px"
+              p="8px" src= {email} />
+        <Text color='gray'
+        fontSize='20px'>Email</Text>
+        <Text fontSize='25px'>0cabezasbelen0@gmail.com</Text>
+        </div>
+
+        <div class="Direccion">
+        <Img position="relative"
+              top="53px"
+              right="56px"
+              w="40px"
+              bg="#D5223A"
+              borderRadius="18px"
+              p="8px" src= {location} />
+        <Text color='gray'
+        fontSize='20px'>Direccion</Text>
+        <Text fontSize='25px'>Gonzalez catan,Bs As.</Text>
+      </div>
+    </Box>
+
+    <div class="Formulario">
+      <h1>Dejame tu consulta</h1>
+      <div class="Formulario-completo">
+      <div class="NombreEmail">
+      <input placeholder="Nombre" type="text" />
+      <input placeholder="Email" type="email"/>
+    </div>
+    <div class="NumeroPresupuesto">
+      <input placeholder="Numero" type="tel"/>
+      <input placeholder="Presupuesto" type="text"/>
+    </div>
+
+    <input class="Mensaje" placeholder="Mensaje" minlength="10" type="text"/> 
+    <div class="Boton">
+       <button>Enviar</button>
+    </div>
+    </div>
+    </div>
+      
+    </Flex>
+    
     </Box>
   );
 };
--- tpfinal/src/Components/Portfolio/Portfolio.jsx
+++ tpfinal/src/Components/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import tpo1b from "../../assets/Imagenes/tpo 1b.png";
 import tpoa from "../../assets/Imagenes/tpo a.png";
 import tpob from "../../assets/Imagenes/tpo b.png";
 import "@fontsource/cactus-classical-serif";
-import '@fontsource/roboto';
+import "@fontsource/roboto";
 import {
   Tabs,
   TabPanels,
@@ -21,70 +21,115 @@ import {
 
 const Portfolio = () => {
   return (
-    <Box><Flex flexDirection='column'
-    bg="#19142A"
-    color="white"
-    p="40px"
-    fontFamily="cactus-classical-serif"
-    fontWeight='lighter'
-    as="Portafolio">
+    <Box>
+      <Flex
+        flexDirection="column"
+        bg="#19142A"
+        color="white"
+        p="40px"
+        fontFamily="cactus-classical-serif"
+        fontWeight="lighter"
+        as="Portafolio"
+      >
         <Text fontSize="50px">Portafolio</Text>
         <Text margin="20px 0px" fontSize="20px">
           Estos son algunos de mis trabajos
         </Text>
       </Flex>
-    <Flex
-      justifyContent="space-around"
-      bg="#19142A"
-      color="white"
-      p="40px"
-      fontFamily="cactus-classical-serif"
-    >
-      
+      <Flex
+        justifyContent="space-around"
+        bg="#19142A"
+        color="white"
+        p="40px"
+        fontFamily="cactus-classical-serif"
+      >
+        <Tabs variant="unstyled">
+          <TabList>
+            <Tab
+              p="10px"
+              fontSize="20px"
+              bg="#1D182F"
+              _selected={{ color: "white", bg: "#D6223B" }}
+            >
+              Todos los trabajos
+            </Tab>
+            <Tab
+              p="10px"
+              fontSize="20px"
+              bg="#1D182F"
+              _selected={{ color: "white", bg: "#D6223B" }}
+            >
+              Proyecto personal
+            </Tab>
+            <Tab p="10px" fontSize="20px" bg="#1D182F" isDisabled>
+              Proyecto profesional
+            </Tab>
+          </TabList>
 
-      <Tabs variant='unstyled'  >
-        <TabList >
-          <Tab p='10px' fontSize='20px' bg='#1D182F' _selected={{ color: "white", bg: "#D6223B" }}>
-            Todos los trabajos
-          </Tab>
-          <Tab p='10px' fontSize='20px' bg='#1D182F' _selected={{ color: "white", bg: "#D6223B" }}>
-            Proyecto personal
-          </Tab>
-          <Tab p='10px' fontSize='20px' bg='#1D182F' isDisabled>Proyecto profesional</Tab>
-        </TabList>
-        
-        <TabPanels>
-          <TabPanel>
-            <Flex justifyContent='center'
-            flexWrap='wrap'>
-            < Image p='10px 5px' alt="Primer pagina web" boxSize="xl"  src={tpo1} />
-            <Image p='10px 5px' alt="Seccion de arte" boxSize="xl"  src={tpo1b} />
-            <Image p='10px 5px' alt="Copia de servicios" boxSize="xl"  src={tpoa} />
-            <Image p='10px 5px' alt="Copia de registro" boxSize="xl"  src={tpob} /></Flex>
-          </TabPanel>
-          <TabPanel>
-          <Flex justifyContent='center'
-            flexWrap='wrap'>
-            <Image p='10px 5px' alt="Copia de servicios" boxSize="xl"  src={tpoa} />
-            <Image p='10px 5px' alt="Copia de registro" boxSize="xl"  src={tpob} /></Flex>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-      
-   </Flex>
-    <Flex justifyContent='center'
-    bg="#19142A">
-      <Button _hover={{ bg: "#D6223B" }} color='white'
-      fontFamily='roboto'
-      bg= "#D6223B"
-      fontSize='30px'
-      fontWeight='lighter'
-      p='40px'
-      mt='55px'
-      mb='70px'
-      >Ver todos</Button>
+          <TabPanels>
+            <TabPanel>
+              <Flex justifyContent="center" flexWrap="wrap">
+                <Image
+                  p="10px 5px"
+                  alt="Primer pagina web"
+                  boxSize="xl"
+                  src={tpo1}
+                />
+                <Image
+                  p="10px 5px"
+                  alt="Seccion de arte"
+                  boxSize="xl"
+                  src={tpo1b}
+                />
+                <Image
+                  p="10px 5px"
+                  alt="Copia de servicios"
+                  boxSize="xl"
+                  src={tpoa}
+                />
+                <Image
+                  p="10px 5px"
+                  alt="Copia de registro"
+                  boxSize="xl"
+                  src={tpob}
+                />
+              </Flex>
+            </TabPanel>
+            <TabPanel>
+              <Flex justifyContent="center" flexWrap="wrap">
+                <Image
+                  p="10px 5px"
+                  alt="Copia de servicios"
+                  boxSize="xl"
+                  src={tpoa}
+                />
+                <Image
+                  p="10px 5px"
+                  alt="Copia de registro"
+                  boxSize="xl"
+                  src={tpob}
+                />
+              </Flex>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Flex>
-  </Box>
+      <Flex justifyContent="center" bg="#19142A">
+        <Button
+          _hover={{ bg: "#D6223B" }}
+          color="white"
+          fontFamily="roboto"
+          bg="#D6223B"
+          fontSize="30px"
+          fontWeight="lighter"
+          p="40px"
+          mt="55px"
+          mb="70px"
+        >
+          Ver todos
+        </Button>
+      </Flex>
+    </Box>
   );
 };
 
