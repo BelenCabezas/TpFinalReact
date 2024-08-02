d732b24b3499ebc7c54827d2fb7ac5665b0cd82c
Belen <0cabezasbelen0@gmail.com>
2024-08-01 21:50:59 -0300

    completando inputs

--- tpfinal/src/Components/Formulario/Formulario.jsx
+++ tpfinal/src/Components/Formulario/Formulario.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import telefono from "../../assets/Iconos/telefono.png";
 import email from "../../assets/Iconos/email.png";
 import location from "../../assets/Iconos/location.png";
-import { Box, Button, Flex, Text,Img } from "@chakra-ui/react";
+import { Box, Button, Flex, Text,Img,Stack,Input } from "@chakra-ui/react";
 import "@fontsource/dm-serif-display";
 
 const Formulario = () => {
@@ -24,7 +24,7 @@ const Formulario = () => {
       fontWeight='10px'>
         Hagamos algo nuevo,diferente y mas significativo o hamos algo mas visual o conceptual?</Text>
 
-     <div class="Llamame">
+     {/* <div class="Llamame">*/}
       <Img position="relative"
               top="53px"
               right="56px"
@@ -35,9 +35,9 @@ const Formulario = () => {
         <Text color='gray'
         fontSize='20px'>Llamame</Text>
         <Text fontSize='25px'>+541167228883</Text>
-     </div>
+     {/*</div>*/}
 
-      <div class="Email">
+      {/*<div class="Email">*/}
         <Img position="relative"
               top="53px"
               right="56px"
@@ -48,9 +48,9 @@ const Formulario = () => {
         <Text color='gray'
         fontSize='20px'>Email</Text>
         <Text fontSize='25px'>0cabezasbelen0@gmail.com</Text>
-        </div>
+       { /*</div>*/}
 
-        <div class="Direccion">
+        {/*<div class="Direccion">*/}
         <Img position="relative"
               top="53px"
               right="56px"
@@ -61,28 +61,41 @@ const Formulario = () => {
         <Text color='gray'
         fontSize='20px'>Direccion</Text>
         <Text fontSize='25px'>Gonzalez catan,Bs As.</Text>
-      </div>
+     { /*</div>*/}
     </Box>
 
-    <div class="Formulario">
-      <h1>Dejame tu consulta</h1>
+    <Flex as="Formulario"
+    flexDir='column'
+    mb='80px'>
+      <Text 
+      fontSize='40px'
+      fontFamily='dm-serif-display'
+      pb='20px'>Dejame tu consulta</Text>
       <div class="Formulario-completo">
       <div class="NombreEmail">
-      <input placeholder="Nombre" type="text" />
-      <input placeholder="Email" type="email"/>
+       <Flex 
+        m='10px'
+        borderRadius='5px'>
+      <Input border='solid gray 1px'  variant='outline' placeholder="Nombre" type="text"/> 
+      <Input border='solid gray 1px' variant='outline' placeholder="Email" type="email"/>
+     </Flex>
+      
     </div>
-    <div class="NumeroPresupuesto">
-      <input placeholder="Numero" type="tel"/>
-      <input placeholder="Presupuesto" type="text"/>
+    <div class="NumeroPresupuesto"> 
+    <Flex 
+    m='10px'
+    borderRadius='5px'>
+      <Input  border='solid gray 1px' variant='outline' placeholder="Numero" type="tel"/>
+      <Input border='solid gray 1px' variant='outline' placeholder="Presupuesto" type="text"/>
+      </Flex>
     </div>
-
-    <input class="Mensaje" placeholder="Mensaje" minlength="10" type="text"/> 
+    <Input border='solid gray 1px' variant='outline' placeholder="Mensaje" minlength="10" type="text"/>
+  
     <div class="Boton">
        <button>Enviar</button>
     </div>
     </div>
-    </div>
-      
+    </Flex> 
     </Flex>
     
     </Box>
